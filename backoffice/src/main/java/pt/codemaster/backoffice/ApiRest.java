375aadad7188ece015f98034936077e62853b439
Felipe <felipemarques1986@gmail.com>
2021-11-23 18:34:22 +0100

    Scaffolding

--- backoffice/src/main/java/pt/codemaster/backoffice/ApiRest.java
+++ backoffice/src/main/java/pt/codemaster/backoffice/ApiRest.java
@@ -3,10 +3,27 @@ package pt.codemaster.backoffice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 @RestController
 public class ApiRest {
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @GetMapping(value = "/analytics.json", produces = APPLICATION_JSON)
+    public String analytics() {
+        return "{}";
+    }
+
+    @GetMapping(value = "/config.json", produces = APPLICATION_JSON)
+    public String config() {
+        return "{}";
+    }
+
+    @GetMapping(value = "/deployment", produces = APPLICATION_JSON)
+    public String deployment() {
+        return "{}";
     }
 }
