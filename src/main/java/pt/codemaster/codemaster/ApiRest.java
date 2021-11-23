ca954d91418f3c054ea182d1737e2b448ee69839
Felipe <felipemarques1986@gmail.com>
2021-11-23 17:38:45 +0100

    Initial commit

--- src/main/java/pt/codemaster/codemaster/ApiRest.java
+++ src/main/java/pt/codemaster/codemaster/ApiRest.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiRest {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/ping")
     public String ping() {
         return "pong";
