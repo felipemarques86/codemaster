c16f5e5c87853205a2083ac04815c911107b7437
Felipe <felipemarques1986@gmail.com>
2021-11-23 16:31:22 +0100

    Initial commit

---
+++ src/test/java/pt/codemaster/codemaster/CodemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.codemaster.codemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
