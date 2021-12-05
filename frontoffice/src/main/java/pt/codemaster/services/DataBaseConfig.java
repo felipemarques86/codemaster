3cc49bded5ddd4696a506df143ab91eca8e97272
Felipe <felipemarques1986@gmail.com>
2021-12-05 21:41:31 +0100

    config

--- frontoffice/src/main/java/pt/codemaster/services/DataBaseConfig.java
+++ frontoffice/src/main/java/pt/codemaster/services/DataBaseConfig.java
@@ -1,17 +1,18 @@
 package pt.codemaster.services;
 
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
 
 @Configuration
 public class DataBaseConfig {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
 
     @Bean
     public DataSource dataSource() {
+        String dbUrl = System.getenv("DATABASE_URL");
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
         return new HikariDataSource(config);
